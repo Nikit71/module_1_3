48f20ff002ab66a7d54a5b478139ac95ac5a81dc
Nikit71 <nikitural@gmail.com>
2024-10-12 21:41:57 +0300

    Add files via upload

---
+++ module_1_3_2.py
@@ -0,0 +1,7 @@
+#Переменные
+print('Практическое задание по уроку "Переменные"')
+number_of_completed_tasks=12
+number_of_hours_spent=1.5
+name_kurs='Python'
+time_to_complete_one_task=number_of_hours_spent/number_of_completed_tasks
+print('Курс:', name_kurs, ', всего задач:', number_of_completed_tasks, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения ', time_to_complete_one_task)
